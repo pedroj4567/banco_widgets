3d658dd66c1b08b6283ce9d2087a773216965262
pedroj4567 <pedroj4567@gmail.com>
2025-04-26 17:07:05 -0500

    creamos item para el menu

---
+++ lib/config/menu/manu_items.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart' show IconData, Icons;
+// con el show solo tomo lo necesario del paquete
+
+// elemento del meniu
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Botones',
+    subtitle: 'Varios Botones',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItem(
+    title: 'Tarjetas',
+    subtitle: 'Un contenedor estilizado',
+    link: '/card',
+    icon: Icons.credit_card,
+  ),
+];
