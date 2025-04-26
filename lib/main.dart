d85458c8a154936647a0b7e393865bb439e94215
pedroj4567 <pedroj4567@gmail.com>
2025-04-26 17:07:33 -0500

    Agregamos el homeScreen como pagina principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:widget_application/presentation/screens/home/home_screen.dart';
+
+import 'config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 1).getTheme(),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: const HomeScreen(),
       ),
     );
   }
