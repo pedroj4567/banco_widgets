b8f29269d1801d38aaf47075247e36b772bb8d77
pedroj4567 <pedroj4567@gmail.com>
2025-04-26 22:32:30 -0500

    Route with name implement

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:widget_application/presentation/screens/cards/card_screen.dart';
 
 import '../../../config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  // para los nombres de navegacion, creamos una propieudad estatica
+
+  static const String name = 'home_screen';
 
   @override
   Widget build(BuildContext context) {
@@ -76,6 +81,7 @@ class _CustomListTitle extends StatelessWidget {
         // usandod goRoutes, extendie en el context
         // lo que pasa es que goRoute, extiende al contexto su metodos para usarse
         context.push(menuItem.link);
+        // context.pushNamed(CardScreen.name); // usamos el nombre de la pantalla a navegar mediante una propiuedad estatica
       },
     );
   }
